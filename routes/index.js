c5a3e32aae33362072d9a90f75bc843536d512ba
Douha <douhadal9@gmail.com>
2020-01-06 19:49:56 +0100

    Douha push #1
    J'ai crée une page spéciale "auth.js" pour les routes de signup et login
    J'ai fait passport
    ...mais ça ne marche pas :)

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 
-const User = require('../models/user')
-
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', {
+    user: req.user,
+  });
 });
-
-/* sign up page*/
-router.get('/signup', (req, res, next) => {
-  res.render('authentification/signup');
-});
-
-router.post('/signup', (req, res, next) =>{
-  let username = req.body.username
-  let email    = req.body.email;
-  let password = req.body.password;
-  
-  const user = new User({
-    username,
-    email,
-    password
-  })
-
-  user.save()
-  .then(user => {
-    res.redirect('./users/create-profile') //revoir lien 
-  })
-  .catch(err => {
-    res.render('/authentification/signup'); //revoir lien 
-  })
-}); 
-
-/* LOGIN page*/
-router.get('/login', (req, res, next) => {
-  res.render('authentification/login');
-});
-
-router.post('/login', (req, res, next) =>{
-  let username = req.body.username
-  let password = req.body.password;
-  
-  const user = new User({
-    username,
-    password
-  })
-
-  user.save()
-  .then(user => {
-    res.redirect('./timeline')  
-  })
-  .catch(err => {
-    res.render('login'); 
-  })
-}); 
 
 module.exports = router;
